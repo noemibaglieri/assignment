c92a4c21088cf259c141737c35b0eeca96e464a9
Noemi Baglieri <noemibaglieri@gmail.com>
2022-05-19 19:06:51 +0100

    responsive edits

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,9 @@
 mybutton = document.getElementById("scrollToTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -26,24 +28,26 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-function validateForm() {
-    var inputName = document.forms["contactForm"]["fname"].value.trim();
-    if (inputName == "") {
-      alert("Please, fill out your name.");
-      return;
-    }
-
-    var inputEmail = document.forms["contactForm"]["femail"].value.trim();
-    var inputNumber = document.forms["contactForm"]["fnumber"].value.trim();
-    if(inputEmail == "" && inputNumber == "") {
-        alert("Please enter either an email or a phone number, so I can get back to you!");
-        return;
-    }
-
-    var input = document.forms["contactForm"]["fmessage"].value.trim();
-    if (input == "") {
-      alert("You didn't write any message!");
-      return;
-    }
-
+function validateForm(event) {
+  event.preventDefault();
+  var inputName = document.forms["contactForm"]["fname"].value.trim();
+  if (!inputName) {
+    alert("Please, fill out your name.");
+    return;
   }
+
+  var inputEmail = document.forms["contactForm"]["femail"].value.trim();
+  var inputNumber = document.forms["contactForm"]["fnumber"].value.trim();
+  if (!inputEmail && !inputNumber) {
+    alert(
+      "Please enter either an email or a phone number, so I can get back to you!"
+    );
+    return;
+  }
+
+  var input = document.forms["contactForm"]["fmessage"].value.trim();
+  if (!input) {
+    alert("You didn't write any message!");
+    return;
+  }
+}
